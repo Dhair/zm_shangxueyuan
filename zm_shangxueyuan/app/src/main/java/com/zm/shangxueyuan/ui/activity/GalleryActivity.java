3a5440180f47e05a6a7f5ef942f37e1c4b09f20d
dengshengjin <dengshengjin@conew.com>
2016-04-20 12:34:11 +0800

    commit photoview

---
+++ zm_shangxueyuan/app/src/main/java/com/zm/shangxueyuan/ui/activity/GalleryActivity.java
@@ -0,0 +1,27 @@
+package com.zm.shangxueyuan.ui.activity;
+
+/**
+ * Creator: dengshengjin on 16/4/20 08:48
+ * Email: deng.shengjin@zuimeia.com
+ */
+public class GalleryActivity extends AbsActionBarActivity {
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initWidgets() {
+
+    }
+
+    @Override
+    protected void initWidgetsActions() {
+
+    }
+
+    @Override
+    protected int getContentView() {
+        return 0;
+    }
+}
---
+++ zm_shangxueyuan/app/src/main/java/com/zm/shangxueyuan/ui/activity/SearchActivity.java
@@ -0,0 +1,27 @@
+package com.zm.shangxueyuan.ui.activity;
+
+/**
+ * Creator: dengshengjin on 16/4/20 08:49
+ * Email: deng.shengjin@zuimeia.com
+ */
+public class SearchActivity extends AbsActionBarActivity {
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initWidgets() {
+
+    }
+
+    @Override
+    protected void initWidgetsActions() {
+
+    }
+
+    @Override
+    protected int getContentView() {
+        return 0;
+    }
+}
